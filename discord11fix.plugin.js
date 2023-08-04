eea42b96a29303c4790053502acd05ad53a7b311
luk <92756992+LukIsHere@users.noreply.github.com>
2023-08-04 19:09:39 +0200

    more readable

--- discord11fix.plugin.js
+++ discord11fix.plugin.js
@@ -3,55 +3,76 @@
  * @author lukistaken
  * @authorLink https://github.com/lukIsHere
  * @invite PwmVfK94zc
- * @version 1.0.1
+ * @version 1.0.2
  * @description Makes new bouble messages work
  * @website https://github.com/LukIsHere/Discord11fix
  * @source https://raw.githubusercontent.com/LukIsHere/Discord11fix/main/discord11fix.plugin.js
  * @updateUrl https://raw.githubusercontent.com/LukIsHere/Discord11fix/main/discord11fix.plugin.js
  */
 
+//yo i put some effort into makin it readable
 module.exports = class MyPlugin {
-  id;
+  currentUserId;
+  isCurrentUserMessage(m){
+    var out = false;
+    
+    m.querySelectorAll("img").forEach(av => {//get all images message element has
+      if (!av.classList.contains("avatar-2e8lTP"))//check if message is pfp
+        return;
+      
+      //this is how pfp link looks like
+      //https://cdn.discordapp.com/avatars/537649475494215690/70bebfde8db1a02799a7622783150bc3.webp?size=80
+      //537649475494215690 is id of user
+      //kinda clever solution i guess
+      var id = av.src.split("/")[4]
+      if (id != this.currentUserId)
+        return;
+
+      m.setAttribute("data-is-author-self", "true");
+      out = true;
+    })
+
+    return out;
+  }
+  updateMessages(){
+    var prev = false;
+    document.querySelectorAll("div.message-2CShn3").forEach(m => {//get all messages
+      if (m.classList.contains("groupStart-3Mlgv1"))//does message contain pfp
+        prev = this.isCurrentUserMessage(m)
+      else if(prev)//was previous message current user message
+        m.setAttribute("data-is-author-self", "true");
+    })
+  }
+  //object listens for changes inside specyfic element
   observe = new MutationObserver((mutationsList, observer) => {
-    //groupStart-3Mlgv1 - OwO this message contains avatar
-    //message-2CShn3 - TwT but with only this it doesn't
     mutationsList.forEach((mutation) => {
       mutation.addedNodes.forEach(n => {
+        //parent element of added element
         var p = n.parentElement;
-        if (p.classList.contains("scrollerContent-2SW0kQ")||p.classList.contains("scrollerInner-2PPAp2")) {
-          var prev = false;
-          p.querySelectorAll("div.message-2CShn3").forEach(m => {
-            if (m.classList.contains("groupStart-3Mlgv1")) {
-              var avv = m.querySelectorAll("img");
-              prev = false;
-              avv.forEach(av => {
-                if (!av.classList.contains("avatar-2e8lTP"))
-                  return;
-                var id = av.src.split("/")[4]
-                if (id == this.id) {
-                  m.setAttribute("data-is-author-self", "true");
-                  prev = true;
-                }
-              })
-            } else {
-              if (prev) m.setAttribute("data-is-author-self", "true");
-            }
-          })
-        }
+        
+        if (p.classList.contains("scrollerContent-2SW0kQ"))//when we switch/move in chat
+          this.updateMessages()
+        if(p.classList.contains("scrollerInner-2PPAp2"))//when we send a message
+          this.updateMessages();
       })
     });
   });
   constructor() {
-    //UwU
+    //UwU nothin here
   }
   start() {
-    this.id = window.__SENTRY__.hub._stack[0].scope._user.id;//>w< don't ask where i got this location from
+    //>w< don't ask where i got this location from
+    //it just is id of current user
+    this.currentUserId = window.__SENTRY__.hub._stack[0].scope._user.id;
 
+    //some configuration for observer
     const config = { childList: true, subtree: true };
 
+    //we start observing
     this.observe.observe(document.getElementsByClassName("base-2jDfDU")[0], config)
   }
   stop() {
+    //we stop observing
     this.observe.disconnect();
   }
 };
